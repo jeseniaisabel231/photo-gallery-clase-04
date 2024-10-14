d49151081ac4acb743f14832fd30f07a15aaacf8
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-10-14 17:42:04 -0500

    Clase 4

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'photo-gallery-clase-04',
+  webDir: 'www'
+};
+
+export default config;
