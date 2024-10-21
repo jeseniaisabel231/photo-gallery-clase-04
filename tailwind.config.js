0382a2a4f0313267f8c1a439156773388360f754
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-10-21 17:35:08 -0500

    terminado

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}', // Archivos HTML y TypeScript en Ionic
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
