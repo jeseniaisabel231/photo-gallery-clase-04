2a06a3191ba58e9a631d9226767dd2d426570a6e
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-10-23 20:46:49 -0500

    ,,

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,22 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
+  //propiedad de la clase llamada resultado, que es de tipo string. Se inicializa con una cadena vacía (''). Esta propiedad se utiliza para almacenar el texto o los símbolos que el usuario va introduciendo, y también se actualiza con el resultado de cualquier cálculo.
   resultado: string = '';
 
   constructor() {}
-
+  //append funcion, agrega símbolos o números al final de la cadena almacenada en resultado, permitiendo que el contenido crezca a medida que el usuario interactúa con la aplicación
   append(symbol: string) {
     this.resultado += symbol;
   }
 
   clear() {
+    // this es necesario para acceder a las propiedades y métodos de la clase
     this.resultado = '';
   }
 
   calculate() {
     try {
       this.resultado = eval(this.resultado);
+      // evalúa el contenido de this.resultado, que contiene una expresión matemática como cadena (por ejemplo, "2+3*4").
     } catch (e) {
       this.resultado = 'Error';
     }
